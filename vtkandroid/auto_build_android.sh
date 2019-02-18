95168a007bf331a736a8eb936b72befd6a256e3c
Michael Pan <panyingyun@gmail.com>
2019-02-18 14:02:39 +0800

    add vtk-android.tar.gz

--- vtkandroid/auto_build_android.sh
+++ vtkandroid/auto_build_android.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-cmake -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake  \
+cmake  -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake  \
 -DCMAKE_ANDROID_NDK=$NDK_ROOT \
--DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_VERBOSE_MAKE=ON \
+-DCMAKE_SYSTEM_NAME=Android \
 -DCMAKE_BUILD_TYPE=Release \
--DCMAKE_CXX_FLAGS='-fpic -fexceptions -frtti' \
+-DCMAKE_CXX_FLAGS='-std=c++11 -fpic -fexceptions -frtti' \
 -DCMAKE_C_FLAGS='-fpic' \
 -DCMAKE_ANDROID_STL_TYPE='c++_static' \
 -DCMAKE_SYSTEM_VERSION=24 \
 -DVTK_ANDROID_BUILD=ON \
--DBUILD_EXAMPLES=ON \
 -DANDROID_NATIVE_API_LEVEL=24 \
 -DOPENGL_ES_VERSION=3.0 ..
 
-make -j8
+cmake --build . 
 
